19735104c2861ee46005b06ebc8976c05b731c60
Nathanael <nathanaeltimotius3@gmail.com>
2024-03-07 20:08:43 +0700

    git preparation

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	// router.HandleFunc("/login", controllers.Login).Methods("POST")
+
+	http.Handle("/", router)
+	fmt.Println("Connected to port 8888")
+	log.Println("Connected to port 8888")
+	log.Fatal(http.ListenAndServe(":8888", router))
+}
